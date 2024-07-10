df6a13005c13a624bac43acd2d40b1a51ef88b25
ogechi <princewillogechi7@gmail.com>
2024-07-10 04:44:44 -0700

    resposive

--- src/components/contact/ContactLeft.jsx
+++ src/components/contact/ContactLeft.jsx
@@ -4,7 +4,7 @@ import { contactImg } from "../../assets/index";
 
 const ContactLeft = () => {
   return (
-    <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
+    <div className="w- lg:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
       <img
         className="w-full h-64 object-cover rounded-lg mb-2"
         src={contactImg}
@@ -21,10 +21,10 @@ const ContactLeft = () => {
         creative mindset. 
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
-          Phone: <span className="text-lightText">+234 8124783577</span>
+          Phone: <span className="text-lightText">+2348124783577</span>
         </p>
-        <p className="text-base text-gray-400 flex items-center gap-2">
-          Email: <span className="text-lightText">princewillogechi7@gmail.com</span>
+        <p className="text-base text-gray-400 inline-block  lg:flex-row items-center gap-2">
+          Email: <span className="text-lightText">princewillogechi7 <br />@gmail.com</span>
         </p>
       </div>
       <div className="flex flex-col gap-4">
