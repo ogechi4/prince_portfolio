64c635426a1dc4de2412f182d14ee651c4dc07b9
ogechi <princewillogechi7@gmail.com>
2024-10-08 12:01:14 -0700

    added new project

--- src/components/banner/Media.jsx
+++ src/components/banner/Media.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact } from "react-icons/fa";
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact, FaInstagram } from "react-icons/fa";
 import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
 const Media = () => {
   return (
@@ -9,15 +9,15 @@ const Media = () => {
             Find me in
           </h2>
           <div className="flex gap-4">
-            <span className="bannerIcon">
-              <FaFacebookF />
-            </span>
-            <span className="bannerIcon">
-              <FaTwitter />
-            </span>
-            <span className="bannerIcon">
-              <FaLinkedinIn />
-            </span>
+          <a href="https://www.facebook.com/profile.php?id=100082148984915" target="_blank" rel="noopener noreferrer" className="bannerIcon">
+             <FaFacebookF />
+           </a>
+           <a href="https://www.instagram.com/princewill_ogechi?igsh=eTlmNmxtOWxuNG9n" target="_blank" rel="noopener noreferrer" className="bannerIcon">
+             <FaInstagram />
+           </a>
+           <a href="https://x.com/ogechimanu521?t=mebqETTVnaycxEcRyL3FNA&s=09" target="_blank" rel="noopener noreferrer" className="bannerIcon">
+             <FaTwitter />
+           </a>
           </div>
         </div>
         <div>
